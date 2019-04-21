f7df7a1c8d5718b6ce68def848d339589b35a65b
Thavenesh Reddy <thavenesh.reddy@thesunexchange.com>
2019-04-21 02:38:28 +0200

    Added event emission upon creation of the BettitEvent contract.

--- contract/contracts/Bettit.sol
+++ contract/contracts/Bettit.sol
@@ -3,11 +3,20 @@ import "./BettitEvent.sol";
 
 contract Bettit {
     int contractsCreated = 0;
+    BettitEvent[] bettitEventContracts;
 
     constructor() public {
     }
 
+    event newBettitEventContract(
+        address contractAddress
+    );
+
     function createEvent(uint256 expiryDate, string memory allowedOutcome1, string memory allowedOutcome2) public returns(address) {
-        return address(new BettitEvent(expiryDate, allowedOutcome1, allowedOutcome2));
+        BettitEvent bettitEvent = new BettitEvent(expiryDate, allowedOutcome1, allowedOutcome2);
+        bettitEventContracts.push(bettitEvent);
+        address lastContractAddress = address(bettitEvent);
+        emit newBettitEventContract(lastContractAddress);
+        return lastContractAddress;
     }
 }
