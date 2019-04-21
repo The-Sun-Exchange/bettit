9b809de24125c837e6d63928e7c067930f608539
Thavenesh Reddy <thavenesh.reddy@thesunexchange.com>
2019-04-21 09:52:12 +0200

    Changed contract address.

--- website/createEvent.js
+++ website/createEvent.js
@@ -64,7 +64,7 @@ function createBettingEvent() {
           ];
 
           let bettitInstanceAddress =
-            "0x077cec2bca715cfeeccaf702c620c00e86fb62de";
+            "0x1e15d2ae7a110f0b14e87ec92944da963d72f619";
 
           const eth = new Eth(web3.currentProvider);
           const contract = new EthContract(eth);
